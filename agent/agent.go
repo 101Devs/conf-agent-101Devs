8958c9015d9efee8b6a76e469d4d6d37b2ceb754
101Devs <113074666+101Devs@users.noreply.github.com>
2022-05-12 11:19:31 +0000

    Back-port of copyright changes to master

--- agent/agent.go
+++ agent/agent.go
@@ -9,4 +9,11 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language go
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package agent
+
+import (
+	"github.com/baidu/conf-agent/conf_reload"
+	"githu
