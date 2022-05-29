7e8acf8d0876561962341cb9d2e5acf8068fda90
101Devs <113074666+101Devs@users.noreply.github.com>
2022-05-29 17:39:46 +0000

    new version of the languages values file

--- build.sh
+++ build.sh
@@ -10,4 +10,10 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governin
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+export PATH=/opt/compiler/gcc-8.2/bin:$PATH
+
+make -f Makefile $1
