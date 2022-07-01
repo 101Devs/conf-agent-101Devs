cfd696a6f6999e2c9f6eae16782eaa03a519937d
101Devs <113074666+101Devs@users.noreply.github.com>
2022-07-01 07:30:18 +0000

    USB: support new huawei devices in option.c

--- conf_reload/prober/prober.go
+++ conf_reload/prober/prober.go
@@ -10,3 +10,13 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
+// limitations under the License.
+
+package prober
+
+import (
+	"context"
+	"time"
+
+	"github.com/baidu/conf-agent/config"
+)
