0857e1c01acffbebed0e5a61f1b6354eee3ca13b
101Devs <113074666+101Devs@users.noreply.github.com>
2022-08-28 19:25:20 +0000

    CLEANUP

--- conf_reload/prober/task_multip_key.go
+++ conf_reload/prober/task_multip_key.go
@@ -1,3 +1,5 @@
 // Copyright (c) 2021 The BFE Authors.
 //
-// Licensed u
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the Licen
