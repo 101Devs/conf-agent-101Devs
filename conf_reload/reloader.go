da54064521d922d4e0ece2555acc06ed35f6476b
101Devs <113074666+101Devs@users.noreply.github.com>
2022-11-07 10:23:58 +0000

    add new template

--- conf_reload/reloader.go
+++ conf_reload/reloader.go
@@ -10,4 +10,14 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License
+// limitations under the License.
+
+package conf_reload
+
+import (
+	"context"
+	"math/rand"
+	"time"
+
+	"github.com/baidu/conf-agent/conf_reload/file_store"
+	"github.com/baidu/conf
