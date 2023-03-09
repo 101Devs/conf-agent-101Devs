175d9e1d322dd5df9418a49cffe221c11a2b5381
101Devs <113074666+101Devs@users.noreply.github.com>
2023-03-09 20:43:40 +0000

    yu

--- config/load.go
+++ config/load.go
@@ -1,4 +1,7 @@
 // Copyright (c) 2021 The BFE Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.o
