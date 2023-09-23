d52ba53d9aa42a14fbee7745d8009fea5b4a1c4a
101Devs <113074666+101Devs@users.noreply.github.com>
2023-09-23 14:15:12 +0000

    remove sass cache

---
+++ docs/zh_cn/README.md
@@ -0,0 +1,3 @@
+# conf-agent-101Devs
+
+## 
--- xlog/logger.go
+++ xlog/logger.go
@@ -20,3 +20,109 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+
+	"github.com/baidu/conf-agent/config"
+	"github.com/baidu/go-lib/log"
+	"github.com/baidu/go-lib/log/log4go"
+)
+
+type Logger interface {
+	Debug(arg0 interface{}, args ...interface{})
+	Info(arg0 interface{}, args ...interface{})
+	Error(arg0 interface{}, args ...interface{}) error
+}
+
+func Init(c *config.LoggerConfig) error {
+	log4go.SetLogBufferLength(10000)
+	log4go.SetLogWithBlocking(false)
+	log4go.SetLogFormat(c.Format)
+
+	logWriter, err := log.Create(c.LogName, c.LogLevel, c.LogDir, c.StdOut, c.RotateWhen, c.BackupCount)
+	if err != nil {
+		return err
+	}
+
+	Default = logWriter
+	return nil
+}
+
+var Default Logger = &fakeLogger{}
+
+type fakeLogger struct{}
+
+func (fl *fakeLogger) Debug(arg0 interface{}, args ...interface{}) {
+	if s, ok := arg0.(string); ok {
+		fmt.Printf(s, args)
+	} else {
+		fmt.Print(arg0, args)
+	}
+	fmt.Println()
+}
+
+func (fl *fakeLogger) Info(arg0 interface{}, args ...interface{}) {
+	if s, ok := arg0.(string); ok {
+		fmt.Printf(s, args)
+	} else {
+		fmt.Print(arg0, args)
+	}
+	fmt.Println()
+}
+
+func (fl *fakeLogger) Error(arg0 interface{}, args ...interface{}) error {
+	if s, ok := arg0.(string); ok {
+		fmt.Printf(s, args)
+	} else {
+		fmt.Print(arg0, args)
+	}
+	fmt.Println()
+	return nil
+}
+
+var ran = rand.NewSource(time.Now().Unix())
+
+var RandomLogID = func() string {
+	return fmt.Sprintf("%d_%03d", time.Now().UnixNano(), ran.Int63()%1000)
+}
+
+type logCtx string
+
+var logCtxKey logCtx = "log_ctx"
+
+type LogContext struct {
+	LogID        string
+	ReloaderName string
+}
+
+func NewContext(ctx context.Context, moduleName string) context.Context {
+	if ctx.Value(logCtxKey) != nil {
+		return ctx
+	}
+
+	return context.WithValue(ctx, logCtxKey, &LogContext{
+		LogID:        RandomLogID(),
+		ReloaderName: moduleName,
+	})
+}
+
+func getLogContext(ctx context.Context) *LogContext {
+	id := ctx.Value(logCtxKey)
+	if id == nil {
+		return &LogContext{}
+	}
+
+	return id.(*LogContext)
+}
+
+func ErrLogFormat(ctx context.Context, topic string, err error) string {
+	logCtx := getLogContext(ctx)
+	return fmt.Sprintf("[%s] module[%16s] topic[%s] err[%v]", logCtx.LogID, logCtx.ReloaderName, topic, err)
+}
+
+func InfoLogFormat(ctx context.Context, topic string, ss ...interface{}) string {
+	logCtx := getLogContext(ctx)
+	if len(ss) > 0 {
+		return fmt.Sprintf("[%s] module[%16s] topic[%s] info[%s]", logCtx.LogID, logCtx.ReloaderName, topic, fmt.Sprint(ss...))
+	}
+
+	return fmt.Sprintf("[%s] module[%16s] topic[%s]", logCtx.LogID, logCtx.ReloaderName, topic)
+}
